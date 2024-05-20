0c8edceba247eb0493d3a049eb41aba7ab138e17
Sarah Le Net <sarahlunette@users.noreply.github.com>
2024-05-20 21:15:51 +0200

    Create main.py
    Draft de l'appli en super simple

---
+++ trajectories/app/main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+#un get the data, randomly sur des indices qu'on n'a pas encore
+#un display de la trajectoire avec pêche supposée en fonction de la vitesse
+#un bouton qui nous permette de labeller: retourner un nouveau record avec la trajectoire et 0 ou 1 à inscrire dans une nouvelle base de données (BQ)
+
+
+#Start labelling button
+#un bouton si clicked on change d'image et on renvoie 0
+#un bouton si clicked on change d'image et on renvoie 1
+
+st.title("Trajectoires de pêche")
+
+if st.button('Start labelling'):
+  #instantiate
+  #choose a record at random
+  #create plot
+  #st.pyplot(#trajectory)
+  st.button('Fishing')
+  st.button('Not Fishing')
+  if st.button('Fishing'):
+    #add record to csv with 1
+    #change the record
+    #check record is not in the csv
+    #change figure (fig facile de changer)
+    #st.session_state = False (unclick button to check)
+  elif st.button('Not Fishing'):
+    #add record to csv with 0
+    #change the record
+    #check record is not in the csv
+    #change figure (fig facile de changer)
+    #st.session_state = False (unclick button to check)
